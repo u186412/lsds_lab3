1f2a329ba183e08527028b86f719e7848e1da2e3
u186412 <simon.gasion01@estudiant.upf.edu>
2024-03-12 17:49:54 +0100

    first commit

---
+++ src/main/java/edu/upf/MastodonWithState.java
@@ -0,0 +1,31 @@
+package edu.upf;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.StreamingContext;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import com.github.tukaaa.MastodonDStream;
+import com.github.tukaaa.config.AppConfig;
+import com.github.tukaaa.model.SimplifiedTweetWithHashtags;
+
+public class MastodonWithState {
+    public static void main(String[] args) throws InterruptedException {
+        SparkConf conf = new SparkConf().setAppName("Real-time Mastodon With State");
+        AppConfig appConfig = AppConfig.getConfig();
+
+        StreamingContext sc = new StreamingContext(conf, Durations.seconds(10));
+        JavaStreamingContext jsc = new JavaStreamingContext(sc);
+        jsc.checkpoint("/tmp/checkpoint");
+
+        JavaDStream<SimplifiedTweetWithHashtags> stream = new MastodonDStream(sc, appConfig).asJStream();
+
+        // TODO IMPLEMENT ME
+
+        // Start the application and wait for termination signal
+        jsc.start();
+        jsc.awaitTermination();
+    }
+
+}
---
+++ src/main/java/edu/upf/storage/DynamoHashTagRepository.java
@@ -0,0 +1,25 @@
+package edu.upf.storage;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+import com.github.tukaaa.model.SimplifiedTweetWithHashtags;
+
+import edu.upf.model.HashTagCount;
+
+public class DynamoHashTagRepository implements IHashtagRepository, Serializable {
+
+  final static String endpoint = "dynamodb.us-east-1.amazonaws.com";
+  final static String region = "us-east-1";
+
+  @Override
+  public void write(SimplifiedTweetWithHashtags h) {
+    // TODO IMPLEMENT ME
+  }
+
+  @Override
+  public List<HashTagCount> readTop10(String lang) {
+    return Collections.emptyList(); // TODO IMPLEMENT ME
+  }
+}
