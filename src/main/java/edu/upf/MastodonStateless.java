a0adb065563010d2b05bc3cccfc53f2ab84c1b09
guilirancel <115788459+guilirancel@users.noreply.github.com>
2024-03-15 20:51:36 +0100

    Update MastodonStateless.java

--- src/main/java/edu/upf/MastodonStateless.java
+++ src/main/java/edu/upf/MastodonStateless.java
@@ -1,7 +1,10 @@
 package edu.upf;
+import scala.Tuple2;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.streaming.StreamingContext;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
@@ -19,11 +22,16 @@ public class MastodonStateless {
 
                 StreamingContext sc = new StreamingContext(conf, Durations.seconds(10));
                 JavaStreamingContext jsc = new JavaStreamingContext(sc);
+                JavaRDD<String> lines = jsc.sparkContext().textFile(input);
                 jsc.checkpoint("/tmp/checkpoint");
 
                 JavaDStream<SimplifiedTweetWithHashtags> stream = new MastodonDStream(sc, appConfig).asJStream();
 
                 // TODO IMPLEMENT ME
+                JavaPairRDD<String, String> staticrdd = LanguageMapUtils.buildLanguageMap(lines);
+                stream.mapToPair(tweet -> new Tuple2<String, Integer> (tweet.getLanguage(), 1)).reduceByKey((x,y) -> x+y)
+                .transformToPair(rdd->rdd.join(staticrdd).mapToPair(tuple -> new Tuple2 <> (tuple._2._1, tuple._2._2))
+                .sortByKey(false)).mapToPair(x -> x.swap()).print();
 
                 // Start the application and wait for termination signal
                 sc.start();
