0bb7b38ca44b412a58bf24dd3c540c9d28fa2800
u186412 <simon.gasion01@estudiant.upf.edu>
2024-03-15 17:18:24 +0100

    Exercise 5

---
+++ README.txt
@@ -0,0 +1,41 @@
+Large scale distributed systems - lab 3
+u186412 - u186415 - u186615
+
+#EX. 3:
+
+#EX.4:
+
+#EX.5:
+how to run:
+spark-submit --conf spark.driver.extraJavaOptions=-Dlog4j.configuration=file:log4j.properties --class edu.upf.MastodonWithState target/lab3-masto
+don-1.0-SNAPSHOT.jar [language]
+
+example output:
+-------------------------------------------
+Time: 1710519410000 ms
+-------------------------------------------
+(11,FanSided)
+(11,Hindustan Times :press:)
+(9,Factline)
+(6,now playing on RFF)
+(5,Chris Adamski 🤖)
+(5,The Ebbtide)
+(5,The Times Of India :press:)
+(5,Italy 24 Press)
+(4,:rss: NME)
+(4,WSJ news feed)
+(4,Europe Says)
+(4,The Recipe Exchange)
+(4,Playing Games)
+(4,nova)
+(4,Vanuatu GIS Feed)
+(4,Club de TéléMatique :verified:)
+(4,The Hindu :press:)
+(4,Derek)
+(3,Biking Japan)
+(3,Farming Simulator Mods)
+...
+
+
+#EX.6
+We weren't able to complete exercise 6 due to lack of time
--- src/main/java/edu/upf/MastodonWithState.java
+++ src/main/java/edu/upf/MastodonWithState.java
@@ -1,17 +1,33 @@
 package edu.upf;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.Optional;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.StreamingContext;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 import com.github.tukaaa.MastodonDStream;
 import com.github.tukaaa.config.AppConfig;
 import com.github.tukaaa.model.SimplifiedTweetWithHashtags;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MastodonWithState {
     public static void main(String[] args) throws InterruptedException {
+        //Get language from input
+        List<String> argsList = Arrays.asList(args);
+        if (argsList.isEmpty()) {
+            System.out.println("Please provide the necessary input parameter: language");
+            System.exit(0);
+        }
+        String language = argsList.get(0);
+        System.out.println("Starting application with language: " + language);
+
+        //start streaming application (provided code)
         SparkConf conf = new SparkConf().setAppName("Real-time Mastodon With State");
         AppConfig appConfig = AppConfig.getConfig();
 
@@ -21,7 +37,32 @@ public class MastodonWithState {
 
         JavaDStream<SimplifiedTweetWithHashtags> stream = new MastodonDStream(sc, appConfig).asJStream();
 
-        // TODO IMPLEMENT ME
+        //filter the usable data
+        JavaDStream<SimplifiedTweetWithHashtags> languageStream = stream
+                .filter(toot -> { //filter ONLY tweets of specified language
+                    String lang = toot.getLanguage();
+                    return !(lang==null) && lang.equals(language);
+                });
+
+        //count number of toots by each user during time
+        JavaPairDStream<String, Integer> usrTootCount = languageStream
+                .mapToPair(toot -> new Tuple2<>(toot.getUserName(), 1))
+                .filter(pair -> !pair._1.isBlank()) //filter blank username
+                .updateStateByKey((values, state) -> {
+                    Integer tootCount = state.orElse(0);
+                    for(Integer value : values){ //not flattened, iterate over value list
+                        tootCount += value;
+                    }
+                    return Optional.of(tootCount);
+                });
+
+        //sort by counts (descending)
+        JavaPairDStream<Integer, String> sortedTootCount = usrTootCount
+                .mapToPair(Tuple2::swap)
+                .transformToPair(rdd -> rdd.sortByKey(false));
+
+        //display 20 users with most toots
+        sortedTootCount.print(20);
 
         // Start the application and wait for termination signal
         jsc.start();
