33370a7c8d55d4515cdf92f24b313e5f68c3c944
u186412 <simon.gasion01@estudiant.upf.edu>
2024-03-15 07:09:02 +0100

    removed debug

--- src/main/java/edu/upf/MastodonStreamingExample.java
+++ src/main/java/edu/upf/MastodonStreamingExample.java
@@ -8,32 +8,9 @@ import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.StreamingContext;
 import org.apache.spark.streaming.api.java.JavaDStream;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
 public class MastodonStreamingExample {
 
     public static void main(String[] args) {
-
-        // Sample JSON string
-        String jsonString = "{\"name\": \"John\", \"age\": 30}";
-
-        // Create a JsonParser object
-        JsonParser parser = new JsonParser();
-
-        try {
-            // Parse the JSON string into a JsonElement
-            JsonElement jsonElement = parser.parseString(jsonString);
-
-            // Print the parsed JSON element
-            System.out.println("Parsed JSON Element:");
-            System.out.println(jsonElement);
-        } catch (Exception e) {
-            // Handle any parsing exceptions
-            e.printStackTrace();
-        }
 
         SparkConf conf = new SparkConf().setAppName("Real-time Mastodon Example");
         AppConfig appConfig = AppConfig.getConfig();
